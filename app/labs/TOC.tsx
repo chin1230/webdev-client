724ab86bd72eed7ca0bd5e73fcde78fb4d0154de
Bingqiao Qian <qbq3225158534@gmail.com>
2026-09-19 17:13:49 -0400

    update

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -11,27 +11,31 @@ export function TOC() {
           </a>
         </li>
         <li>
-          <Link href="/labs">Home</Link>
+          <Link href="/labs" id="wd-home-link">Home</Link>
         </li>
 
         <li>
-          <Link href="/labs/lab1">Lab 1</Link>
+          <Link href="/labs/lab1" id="wd-lab1-link">Lab 1</Link>
         </li>
 
         <li>
-          <Link href="/labs/lab2">Lab 2</Link>
+          <Link href="/labs/lab2" id="wd-lab2-link">Lab 2</Link>
         </li>
 
         <li>
-          <Link href="/labs/lab3">Lab 3</Link>
+          <Link href="/labs/lab3" id="wd-lab3-link">Lab 3</Link>
         </li>
 
         <li>
-          <Link href="/labs/lab4">Lab 4</Link>
+          <Link href="/labs/lab4" id="wd-lab4-link">
+            Lab 4
+          </Link>
         </li>
 
         <li>
-          <Link href="/labs/lab5">Lab 5</Link>
+          <Link href="/labs/lab5" id="wd-lab5-link">
+            Lab 5
+          </Link>
         </li>
         <li>
           <Link href="/" id="wd-kambaz-link">
--- app/labs/lab4/page.tsx
+++ app/labs/lab4/page.tsx
@@ -1,6 +1,6 @@
 export default function Lab4() {
   return (
-    <div id="wd-lab4">
+    <div id="wd-lab4-link">
       <h2>Lab 4</h2>
       {/* do the next exercise here */}
     </div>
